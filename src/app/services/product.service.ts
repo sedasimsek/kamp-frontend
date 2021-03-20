bd89a40098b79268a1cbd84efd3ed00c8fefd4a4
sedasimsek <sedasimsekk27@gmail.com>
2021-03-21 01:14:24 +0300

    20 Mart Ders Sonu

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,15 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { ListResponseModel } from '../models/listResponseModel';
 import { Product } from '../models/product';
+import { ResponseModel } from '../models/responseModel';
+import { ListResponseModel } from '../models/listResponseModel';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  apiUrl = 'https://localhost:44344/api/';
+  apiUrl = 'https://localhost:44314/api/';
 
   constructor(private httpClient: HttpClient) { }
 
@@ -19,7 +20,11 @@ export class ProductService {
   }
 
   getProductsByCategory(categoryId:number):Observable<ListResponseModel<Product>> {
-    let newPath = this.apiUrl + "products/getbycategory?categoryId"+categoryId
-    return this.httpClient.get<ListResponseModel<Product>>(this.apiUrl);
+    let newPath = this.apiUrl + "products/getbycategory?categoryId="+categoryId
+    return this.httpClient.get<ListResponseModel<Product>>(newPath);
+  }
+
+  add(product:Product):Observable<ResponseModel>{
+    return this.httpClient.post<ResponseModel>(this.apiUrl+"products/add",product)
   }
 }
